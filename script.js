1e6fa4c210c36c975c492d58d2109d8eb336b5f7
sebastianleonchitan <sebasleon20061988@gmail.com>
2026-02-08 13:41:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const noBtn = document.getElementById("no");
 const yesBtn = document.getElementById("yes");
 
-function moveButton() {
-  const padding = 20;
+noBtn.addEventListener("mouseover", moverBoton);
+noBtn.addEventListener("touchstart", moverBoton);
 
+function moverBoton() {
+  const padding = 20;
   const maxX = window.innerWidth - noBtn.offsetWidth - padding;
   const maxY = window.innerHeight - noBtn.offsetHeight - padding;
 
   const x = Math.random() * maxX;
   const y = Math.random() * maxY;
 
+  noBtn.style.position = "fixed";
   noBtn.style.left = `${x}px`;
   noBtn.style.top = `${y}px`;
 }
-
-noBtn.addEventListener("mouseover", moveButton);
-noBtn.addEventListener("touchstart", moveButton);
 
 yesBtn.addEventListener("click", () => {
   document.body.innerHTML = `
@@ -27,13 +27,10 @@ yesBtn.addEventListener("click", () => {
       align-items:center;
       background:linear-gradient(180deg,#ff9a9e,#fad0c4);
       color:white;
-      font-size:2rem;
       text-align:center;
       padding:20px;">
-      <h1>💛 ¡Sabía que dirías que sí! 💛</h1>
-      <p style="margin-top:15px;">
-        Te amo más que un Minion ama los plátanos 🍌
-      </p>
+      <h1> ¡Lo Sabía😞! </h1>
+      <p style="font-size:1.5rem;">Igual Celoso no Soy🥵</p>
     </div>
   `;
 });
